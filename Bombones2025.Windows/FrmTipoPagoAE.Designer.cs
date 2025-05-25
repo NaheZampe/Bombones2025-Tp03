c5856e28e6d9b598a28b15fed0ec3259a342eff6
Nahuel Delia <NahuelDelia@outlook.com>
2025-05-25 20:02:00 -0300

    Se agregaron Formularios de Windows para el manejo de las formas de pago asi como las funciones de sus botones.

---
+++ Bombones2025.Windows/FrmTipoPagoAE.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Bombones2025.Windows
+{
+    partial class FrmTipoPagoAE
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            BtnCancelarPagoAE = new Button();
+            BtnOKPagoAE = new Button();
+            TxtPagoAE = new TextBox();
+            label1 = new Label();
+            errorProvider1 = new ErrorProvider(components);
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
+            SuspendLayout();
+            // 
+            // BtnCancelarPagoAE
+            // 
+            BtnCancelarPagoAE.Image = Properties.Resources.Cancelar;
+            BtnCancelarPagoAE.Location = new Point(373, 71);
+            BtnCancelarPagoAE.Name = "BtnCancelarPagoAE";
+            BtnCancelarPagoAE.Size = new Size(75, 52);
+            BtnCancelarPagoAE.TabIndex = 11;
+            BtnCancelarPagoAE.Text = "Cancelar";
+            BtnCancelarPagoAE.TextImageRelation = TextImageRelation.ImageAboveText;
+            BtnCancelarPagoAE.UseVisualStyleBackColor = true;
+            BtnCancelarPagoAE.Click += BtnCancelarPagoAE_Click;
+            // 
+            // BtnOKPagoAE
+            // 
+            BtnOKPagoAE.Image = Properties.Resources.Aceptar1;
+            BtnOKPagoAE.Location = new Point(135, 71);
+            BtnOKPagoAE.Name = "BtnOKPagoAE";
+            BtnOKPagoAE.Size = new Size(75, 52);
+            BtnOKPagoAE.TabIndex = 12;
+            BtnOKPagoAE.Text = "OK";
+            BtnOKPagoAE.TextImageRelation = TextImageRelation.ImageAboveText;
+            BtnOKPagoAE.UseVisualStyleBackColor = true;
+            BtnOKPagoAE.Click += BtnOKPagoAE_Click;
+            // 
+            // TxtPagoAE
+            // 
+            TxtPagoAE.Location = new Point(135, 32);
+            TxtPagoAE.MaxLength = 200;
+            TxtPagoAE.Name = "TxtPagoAE";
+            TxtPagoAE.Size = new Size(313, 23);
+            TxtPagoAE.TabIndex = 10;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(48, 35);
+            label1.Name = "label1";
+            label1.Size = new Size(80, 15);
+            label1.TabIndex = 9;
+            label1.Text = "Tipo de pago:";
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
+            // FrmTipoPagoAE
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(524, 159);
+            Controls.Add(BtnCancelarPagoAE);
+            Controls.Add(BtnOKPagoAE);
+            Controls.Add(TxtPagoAE);
+            Controls.Add(label1);
+            Name = "FrmTipoPagoAE";
+            Text = "FrmTipoPagoAE";
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button BtnCancelarPagoAE;
+        private Button BtnOKPagoAE;
+        private TextBox TxtPagoAE;
+        private Label label1;
+        private ErrorProvider errorProvider1;
+    }
+}
---
+++ Bombones2025.Windows/FrmTipoPagoAE.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Bombones2025.Entidades.Entidades;
+
+namespace Bombones2025.Windows
+{
+    public partial class FrmTipoPagoAE : Form
+    {
+        private FormaDePago? formaDePago;
+        public FrmTipoPagoAE()
+        {
+            InitializeComponent();
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            if (formaDePago is not null)
+            {
+                TxtPagoAE.Text = formaDePago.Descripcion;
+            }
+        }
+
+        public FormaDePago? GetPago()
+        {
+            return formaDePago;
+        }
+
+        public void SetPago(FormaDePago formaDePago)
+        {
+            this.formaDePago = formaDePago;
+        }
+
+        private void BtnOKPagoAE_Click(object sender, EventArgs e)
+        {
+            if (ValidarDatos())
+            {
+                if (formaDePago is null)
+                {
+                    formaDePago = new FormaDePago();
+
+                }
+                formaDePago.Descripcion = TxtPagoAE.Text;
+
+                DialogResult = DialogResult.OK;
+            }
+        }
+
+        private bool ValidarDatos()
+        {
+            bool valido = true;
+            errorProvider1.Clear();
+            if (string.IsNullOrEmpty(TxtPagoAE.Text))
+            {
+                valido = false;
+                errorProvider1.SetError(TxtPagoAE, "El nombre es requerido");
+
+            }
+            return valido;
+        }
+
+        private void BtnCancelarPagoAE_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+    }
+}
